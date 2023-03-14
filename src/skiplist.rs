da84754371a9db0346182d1d7662d317e5f13041
zhangzelin <zhangzelin@zzl.local>
2023-03-14 19:40:18 +0800

    bench skiplist

--- src/skiplist.rs
+++ src/skiplist.rs
@@ -88,10 +88,10 @@ impl<T: PartialOrd + Display + Default> SkipList<T> {
         if let Some(next_node) = next {
             unsafe {
                 if (*next_node.as_ptr()).key == key {
-                    (*next_node.as_ptr()).key = key
+                    (*next_node.as_ptr()).key = key;
+                    return;
                 }
             }
-            return;
         };
 
         let level = self.get_random_level();
@@ -100,7 +100,7 @@ impl<T: PartialOrd + Display + Default> SkipList<T> {
             next: Vec::with_capacity(self.max_level),
         };
 
-        for _ in 0..=level {
+        for _ in 0..level {
             new_node.next.push(None);
         }
 
@@ -128,9 +128,60 @@ impl<T: PartialOrd + Display + Default> SkipList<T> {
         self.current_len += 1;
     }
 
+    pub fn delete(&mut self, key: T) -> Option<T> {
+        let mut prev = self.head;
+        let mut next = None;
+        for i in (0..self.current_level).rev() {
+            unsafe {
+                next = prev.and_then(|prev_ptr| (*prev_ptr.as_ptr()).next[i]);
+                while let Some(node) = next {
+                    if (*node.as_ptr()).key >= key {
+                        break;
+                    }
+                    prev = next;
+                    next = prev.and_then(|prev_ptr| (*prev_ptr.as_ptr()).next[i]);
+                }
+                self.tmp[i] = prev;
+            }
+        }
+        if let Some(next_node) = next {
+            unsafe {
+                if (*next_node.as_ptr()).key == key {
+                    let target_node = Box::from_raw(next_node.as_ptr());
+                    for i in 0..target_node.next.len() {
+                        match self.tmp[i].take() {
+                            Some(prev_node) => {
+                                println!(
+                                    "delete prev {} {:?} to next {:?}",
+                                    i, prev_node, target_node.next[i]
+                                );
+                                (*prev_node.as_ptr()).next[i] = target_node.next[i];
+                                if prev_node == self.head.unwrap()
+                                    && (*prev_node.as_ptr()).next[i] == None
+                                {
+                                    self.current_level -= 1;
+                                }
+                            }
+                            None => {
+                                println!(
+                                    "BUG: tmp prev {} is None, but current length is {}",
+                                    i,
+                                    target_node.next.len()
+                                );
+                            }
+                        }
+                    }
+
+                    return Some(target_node.key);
+                }
+            }
+        };
+        None
+    }
+
     fn get_random_level(&mut self) -> usize {
-        let mut l = 1;
-        for _ in 1..self.max_level {
+        let mut l = 0;
+        for _ in 0..self.max_level {
             let gen_v: usize = self.rng.gen();
             if gen_v % self.ratio == 0 {
                 l += 1;
@@ -159,8 +210,19 @@ impl<T: Default + PartialOrd + Display> Display for SkipList<T> {
 
 #[cfg(test)]
 mod tests {
+
     use super::*;
 
+    struct Fib(i32, i32);
+
+    impl Iterator for Fib {
+        type Item = i32;
+        fn next(&mut self) -> Option<Self::Item> {
+            Some(self.0 + self.1)
+        }
+    }
+
+    #[derive(Debug)]
     struct TestK {
         k: i32,
         v: i32,
@@ -203,20 +265,61 @@ mod tests {
         l.set(TestK { k: 4, v: 1 });
         println!("{}", l);
         assert_eq!(l.get(TestK { k: 1, v: 0 }).unwrap().v, 1);
+
+        // modify value
+        l.set(TestK { k: 4, v: 7 });
+        println!("{}", l);
+        assert_eq!(l.get(TestK { k: 4, v: 0 }).unwrap().v, 7);
+
+        // delete key
+        assert_eq!(l.delete(TestK { k: 3, v: 0 }), Some(TestK { k: 3, v: 1 }));
+        println!("{}", l);
+        assert_eq!(l.delete(TestK { k: 7, v: 0 }), None);
+        println!("{}", l);
+    }
+
+    //test skiplist::tests::delete_bench ... bench:           2 ns/iter (+/- 0)
+    //test skiplist::tests::get_bench    ... bench:           8 ns/iter (+/- 0)
+    //test skiplist::tests::set_bench    ... bench:      85,710 ns/iter (+/- 5,533)
+    #[bench]
+    fn set_bench(b: &mut test::Bencher) {
+        let mut f = Fib(0, 1).into_iter();
+        let mut l = SkipList::new(32, 4);
+
+        b.iter(|| {
+            let v = f.next().unwrap();
+            for _ in 0..10000 {
+                l.set(TestK { k: v, v });
+            }
+        })
     }
 
     #[bench]
-    fn basic_bench(b: &mut test::Bencher) {
-        let mut r = rand::thread_rng();
-        // 274,301 / 10000 = 27ns/op
-        b.iter(|| {
-            let mut l = SkipList::new(32, 4);
-            for _ in 0..10000 {
-                l.set(TestK {
-                    k: r.gen(),
-                    v: r.gen(),
-                });
-            }
+    fn get_bench(b: &mut test::Bencher) {
+        let mut f = Fib(0, 1).into_iter();
+        let l = &mut SkipList::new(32, 4);
+        for _ in 0..10000 {
+            let v = f.next().unwrap();
+            l.set(TestK { k: v, v });
+        }
+        let mut f = Fib(0, 1).into_iter();
+        b.iter(move || {
+            let v = f.next().unwrap();
+            l.get(TestK { k: v, v });
+        })
+    }
+
+    #[bench]
+    fn delete_bench(b: &mut test::Bencher) {
+        let mut f = Fib(0, 1).into_iter();
+        let l = &mut SkipList::new(32, 4);
+        for _ in 0..10000 {
+            let v = f.next().unwrap();
+            l.set(TestK { k: v, v });
+        }
+        b.iter(move || {
+            let v = f.next().unwrap();
+            l.delete(TestK { k: v, v });
         })
     }
 }
